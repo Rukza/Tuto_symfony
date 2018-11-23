c36e3667306eb297ad50df95b4bf013cbf951359
Kza <Rukza@orange.fr>
2018-11-23 12:14:36 +0100

    Tuto d'un blog sous symfony

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = \Faker\Factory::create('fr_FR');
+        // créer 3 catégories fakées
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+            // créer entre 4 et 6 articles
+
+
+            for ($j = 1; $j <= mt_rand(4, 6); $j++){
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+                //faker donne des paragraphes en tableau du coup il faut les transformer
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+    
+                        $manager->persist($article);
+                
+                // on donne des commentaires à l'article
+                for($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+                    //faire en sorte que la date du commentaire soit normale par rapport a la date de création de l'article
+                    $now = new \DateTime();
+                    $interval = $now->diff($article->getCreatedAt());
+                    $days = $interval->days;
+                    $minimum = '-' . $days . ' days';// -100 days
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimum))
+                            ->setArticle($article);
+
+                            $manager->persist($comment);
+                }       
+            }
+
+
+        }
+
+        
+        $manager->flush();
+    }
+}
